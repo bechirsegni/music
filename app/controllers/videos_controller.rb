b9be3c23ef52d429d77cd3ce6c5e094d44d06b26
Bechir Segni <bechir.dev@gmail.com>
2015-03-31 22:03:48 +0100

    added video controller

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,58 @@
+class VideosController < ApplicationController
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+  @videos = Video.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @video = current_user.videos.build
+  end
+
+  def create
+    @video = current_user.videos.build(video_params)
+    if @video.save
+      redirect_to videos_path, notice: 'Video was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @video.update(video_params)
+      redirect_to video_url, notice: 'Blog was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @video.destroy
+    redirect_to videos_url, notice: 'Video was successfully destroyed.'
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :description, :video_id)
+  end
+
+  def correct_user
+    @video = current_user.videos.find_by(id: params[:id])
+    redirect_to videos_path, notice: "Not authorized to edit this Video" if @video.nil?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330185126) do
+ActiveRecord::Schema.define(version: 20150331202716) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,5 +31,14 @@ ActiveRecord::Schema.define(version: 20150330185126) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "videos", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "user_id",     limit: 4
+    t.string   "video_id",    limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
