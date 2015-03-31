2e9a49dc1701527a4e5959d0194f1253772ae34f
Bechir Segni <bechir.dev@gmail.com>
2015-03-31 20:57:41 +0100

    Add CSS and JS and creating home page plus layout for login pages and fixing devise layout

--- Gemfile
+++ Gemfile
@@ -16,3 +16,4 @@ group :development, :test do
   gem 'spring'
 end
 gem 'devise', '~> 3.4.1'
+gem 'ie_conditional_tag', '~> 0.6.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   protected
+
+  def layout_by_resource
+    if devise_controller?
+      'users'
+    else
+      'application'
+    end
+  end
 
   def configure_permitted_parameters
     registration_params = [:name, :email, :password, :password_confirmation]
